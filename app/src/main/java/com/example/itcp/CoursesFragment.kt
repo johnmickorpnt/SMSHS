eb07f0aeece05ad03c193e8ffefaacba4385d180
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-03-09 11:45:19 +0800

    Small UI Changes
    Small UI Changes

--- app/src/main/java/com/example/itcp/CoursesFragment.kt
+++ app/src/main/java/com/example/itcp/CoursesFragment.kt
@@ -1,59 +1,91 @@
 package com.example.itcp
 
+import android.graphics.Rect
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CoursesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CoursesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var adapter: MyAdapter
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var courseArrayList : ArrayList<CoursesModel>
 
+    private lateinit var title : Array<String>
+    private lateinit var section : Array<String>
+    private lateinit var status : Array<String>
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_courses, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModulesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CoursesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInit()
+        var layoutManager =  StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        newRecyclerView = view.findViewById(R.id.recycler_view)
+        adapter = MyAdapter(courseArrayList)
+        newRecyclerView.adapter = adapter
+        newRecyclerView.isNestedScrollingEnabled = false
+        newRecyclerView.layoutManager = layoutManager
+
+    }
+
+    private fun dataInit(){
+        courseArrayList = arrayListOf<CoursesModel>()
+
+        title = arrayOf(
+            "IT 001",
+            "IT 002",
+            "IT 203",
+            "ITE 404",
+            "IT 005",
+            "ITE 106",
+            "ITE 404",
+            "IT 005",
+            "ITE 106",
+        )
+
+        section = arrayOf(
+            "IT4A1",
+            "IT4A2",
+            "IT4A1",
+            "IT4A2",
+            "IT4A1",
+            "IT4A3",
+            "IT4A2",
+            "IT4A1",
+            "IT4A3"
+        )
+
+        status = arrayOf(
+            "N/A",
+            "100%",
+            "50%",
+            "70%",
+            "80%",
+            "NA",
+            "70%",
+            "80%",
+            "NA",
+        )
+
+        for(i in title.indices){
+            val course = CoursesModel(title[i], section[i], status[i])
+            courseArrayList.add(course)
+        }
     }
 }
